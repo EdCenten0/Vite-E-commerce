92f593b2f1dac6f666d18a5ffe70014b5ad91206
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-10-03 00:43:14 -0600

    Card component

--- Vite E-Commerce/src/pages/Home/index.jsx
+++ Vite E-Commerce/src/pages/Home/index.jsx
@@ -1,9 +1,13 @@
+import { Card } from "../../components/Card";
 import { Layout } from "../../components/Layout";
 
 function Home() {
   return (
     <>
-      <Layout>Home</Layout>
+      <Layout>
+        Home
+        <Card />
+      </Layout>
     </>
   );
 }
