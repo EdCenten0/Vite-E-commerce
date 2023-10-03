a35f981454c2ccce3f033544e130c304663f913a
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-10-02 22:40:06 -0600

    BrowserRoutes for principal pages

--- Vite E-Commerce/src/pages/App/index.jsx
+++ Vite E-Commerce/src/pages/App/index.jsx
@@ -1,9 +1,50 @@
-// import "./App.css";
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import "./App.css";
+
+import Home from "../Home/Index";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-order",
+      element: <MyOrder />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/*",
+      element: <NotFound />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+  ]);
+
+  return routes;
+};
 
 function App() {
   return (
     <>
-      <div className="App">Hola mundo</div>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
   );
 }
