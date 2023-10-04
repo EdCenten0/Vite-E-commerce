2f7852c3092492e02022f08195ba20d7cc1b0c6d
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-10-03 22:54:30 -0600

    Added Api and filling cards

--- Vite E-Commerce/src/components/Card/index.jsx
+++ Vite E-Commerce/src/components/Card/index.jsx
@@ -1,22 +1,22 @@
-function Card() {
+function Card(data) {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sm m-1 px-2">
-          Electronics
+          {data.data.category?.name}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://cdn.thewirecutter.com/wp-content/media/2023/07/bluetoothheadphones-2048px-6141.jpg"
-          alt="headphones"
+          src={data.data.images[0]}
+          alt={data.data.title}
         />
         <div className="absolute m-2 top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full">
           +
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">300$</span>
+        <span className="text-sm font-light"> {data.data.title}</span>
+        <span className="text-lg font-medium"> {data.data.price}$</span>
       </p>
     </div>
   );
--- Vite E-Commerce/src/pages/Home/index.jsx
+++ Vite E-Commerce/src/pages/Home/index.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import { Card } from "../../components/Card";
 import { Layout } from "../../components/Layout";
+import { apiUrl } from "../../api/api";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`);
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // console.log(items);
+
   return (
     <>
       <Layout>
         Home
-        <Card />
+        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+          {items?.map((item) => {
+            return <Card data={item} key={item.id} />;
+          })}
+        </div>
       </Layout>
     </>
   );
