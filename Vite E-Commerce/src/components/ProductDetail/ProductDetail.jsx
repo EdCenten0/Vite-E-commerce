d0a7fcaa79a5b7e2b124d108f464ccc2eb709083
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-10-05 18:58:20 -0600

    Added event to close productdetail

--- Vite E-Commerce/src/components/ProductDetail/ProductDetail.jsx
+++ Vite E-Commerce/src/components/ProductDetail/ProductDetail.jsx
@@ -15,7 +15,10 @@ function ProductDetail() {
       <div className="flex justify-between  items-center p-6">
         <h2 className="font-medium text-xl ">Detail</h2>{" "}
         <div>
-          <XMarkIcon className="h-6 w-6 text-black-500" />
+          <XMarkIcon
+            onClick={context.closeProductDetail}
+            className="h-6 w-6 text-black-500 cursor-pointer animate-pulse"
+          />
         </div>
       </div>
     </aside>
