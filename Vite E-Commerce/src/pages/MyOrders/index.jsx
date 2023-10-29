fd821868e732387d2b641c21ecbbdbfcfcd3cf62
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-10-28 22:27:41 -0600

    An icon was added to show when orders are empty

--- Vite E-Commerce/src/pages/MyOrders/index.jsx
+++ Vite E-Commerce/src/pages/MyOrders/index.jsx
@@ -3,9 +3,40 @@ import { Layout } from "../../components/Layout";
 import { OrdersCard } from "../../components/OrdersCard";
 import { ShoppingCartContext } from "../../contexts";
 import { Link } from "react-router-dom";
+import { EmptyOrdersIcon } from "./EmptyOrdersIcon";
 
 function MyOrders() {
   const context = useContext(ShoppingCartContext);
+  const renderView = () => {
+    if (context.order.length > 0) {
+      return (
+        <>
+          {context.order.map((order, index) => (
+            <Link key={index} to={`/Vite-E-commerce/my-orders/${index}`}>
+              <OrdersCard
+                totalPrice={order.totalPrice}
+                totalProducts={order.totalProducts}
+              />
+            </Link>
+          ))}
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className=" w-full h-full flex justify-center items-center flex-col">
+            <figure className="w-20">
+              <EmptyOrdersIcon />
+            </figure>
+            <div>
+              <p className=" font-semibold">Nothing related :(</p>
+            </div>
+          </div>
+        </>
+      );
+    }
+  };
+
   return (
     <>
       <Layout className="App">
@@ -13,14 +44,7 @@ function MyOrders() {
           <h1>MyOrders</h1>
         </div>
       </Layout>
-      {context.order.map((order, index) => (
-        <Link key={index} to={`/Vite-E-commerce/my-orders/${index}`}>
-          <OrdersCard
-            totalPrice={order.totalPrice}
-            totalProducts={order.totalProducts}
-          />
-        </Link>
-      ))}
+      {renderView()}
     </>
   );
 }
